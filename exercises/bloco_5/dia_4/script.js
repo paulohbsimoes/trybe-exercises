d241d1f270e7563ba92eb3da459284bf0fcf0c64
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-06 08:13:59 -0300

    Ajustes finais

--- exercises/bloco_5/dia_4/script.js
+++ exercises/bloco_5/dia_4/script.js
@@ -1,32 +1,32 @@
 const textContainer = document.querySelector('#texto');
 
-const settings = {
+const initialSettings = {
   backgroundColor: 'orange',
-  textColor: 'red',
+  color: 'white',
   fontSize: '15px',
   lineHeight: '25px',
   fontFamily: 'sans-serif'
 }
 
-function config() {
-  for (let key in settings) {
-    textContainer.style[key] = settings[key];
+const settings = localStorage.settings ? JSON.parse(localStorage.settings) : initialSettings;
+
+function updateConfigs() {
+  for (let cur in settings) {
+    textContainer.style[cur] = settings[cur];
   }
 }
 
-config();
+updateConfigs();
 
 function setConfig(key, val) {
-  localStorage[key] = val;
-}
-
-function getConfig(key) {
-  return localStorage[key];
+  settings[key] = val;
+  updateConfigs();
+  localStorage.setItem('settings', JSON.stringify(settings));
 }
 
 const preferencias = document.querySelector('#preferencias');
 
-preferencias.addEventListener('click', function(event) {
+preferencias.addEventListener('click', function (event) {
   event.stopPropagation();
 })
 
@@ -41,47 +41,46 @@ settingsButton.addEventListener('click', (event) => {
 });
 
 const corDeFundo = document.getElementById('cor-de-fundo');
-corDeFundo.addEventListener('keydown', function(event) {
+corDeFundo.addEventListener('keydown', function (event) {
   if (event.code === 'Enter') {
     textContainer.style.backgroundColor = corDeFundo.value;
+    setConfig('backgroundColor', corDeFundo.value);
     corDeFundo.value = '';
   }
 });
 
 const corDeTexto = document.getElementById('cor-de-texto');
-corDeTexto.addEventListener('keydown', function(event) {
+corDeTexto.addEventListener('keydown', function (event) {
   if (event.code === 'Enter') {
     textContainer.style.color = corDeTexto.value;
+    setConfig('color', corDeTexto.value);
     corDeTexto.value = '';
   }
 });
 
 const tamanhoDaFonte = document.getElementById('tamanho-da-fonte');
-tamanhoDaFonte.addEventListener('keydown', function(event) {
+tamanhoDaFonte.addEventListener('keydown', function (event) {
   if (event.code === 'Enter') {
     textContainer.style.fontSize = tamanhoDaFonte.value;
+    setConfig('fontSize', tamanhoDaFonte.value);
     tamanhoDaFonte.value = '';
   }
 });
 
 const espacamentoEntreLinhas = document.getElementById('espacamento-entre-linhas');
-espacamentoEntreLinhas.addEventListener('keydown', function(event) {
+espacamentoEntreLinhas.addEventListener('keydown', function (event) {
   if (event.code === 'Enter') {
     textContainer.style.lineHeight = espacamentoEntreLinhas.value;
+    setConfig('lineHeight', espacamentoEntreLinhas.value);
     espacamentoEntreLinhas.value = '';
   }
 });
 
 const tipoDeFonte = document.getElementById('tipo-de-fonte');
-tipoDeFonte.addEventListener('keydown', function(event) {
+tipoDeFonte.addEventListener('keydown', function (event) {
   if (event.code === 'Enter') {
     textContainer.style.fontFamily = tipoDeFonte.value;
+    setConfig('fontFamily', tipoDeFonte.value);
     tipoDeFonte.value = '';
   }
 });
-
-console.log(corDeFundo);
-console.log(corDeTexto);
-console.log(tamanhoDaFonte);
-console.log(espacamentoEntreLinhas);
-console.log(tipoDeFonte);
