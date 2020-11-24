1cf1b8e8dad7c3e51473747cf8298b416ddd769c
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-24 11:10:57 -0300

    Parte 1 - Exercicio 1 concluido

---
+++ exercises/bloco_7/dia_1/exercicio_1_1_2.js
@@ -0,0 +1,12 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
