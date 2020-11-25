008e58b9a734319dcb0ec3b9ef58a56df63afc93
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-24 23:16:13 -0300

    Bonus - Exercicio 2 concluido

--- exercises/bloco_7/dia_2/exercicio-parte-2.js
+++ exercises/bloco_7/dia_2/exercicio-parte-2.js
@@ -90,3 +90,26 @@ const countStundentPerLesson = lesson => {
 }
 
 console.log(countStundentPerLesson('Matemática'));
+
+// Bonus 2
+
+const createReport = (obj, professor) => {
+  const report = {
+    professor,
+    aulas: [],
+    estudantes: 0
+  }
+
+  Object.entries(obj)
+    .filter(([_, value]) => value.professor === professor)
+    .map(([_, value]) => value)
+    .forEach(obj => {
+      report.aulas.push(obj.materia);
+      report.estudantes += obj.numeroEstudantes;
+    })
+
+  return report;
+}
+
+console.log(createReport(allLessons, 'Maria Clara'))
+console.log(createReport(allLessons, 'Carlos'))
