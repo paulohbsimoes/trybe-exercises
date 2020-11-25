bab09a0361f396a133f4325791aff121f7bc5782
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-24 23:12:45 -0300

    Parte 2 - Exercicio 1 concluido

---
+++ exercises/bloco_7/dia_2/exercicio-parte-2.js
@@ -0,0 +1,29 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Exercício 1
+
+const addObjProperty = (obj, key, val) => obj[key] = val;
+
+console.log(lesson2);
+
+addObjProperty(lesson2, 'turno', 'manhã');
+
+console.log(lesson2);
