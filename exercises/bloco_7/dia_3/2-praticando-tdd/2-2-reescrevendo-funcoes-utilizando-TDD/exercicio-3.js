4ae33d796fdfbeb60b94babf94ac937db6e64b73
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-26 09:12:04 -0300

    Reescrevendo funcoes utilizando TDD - Exercicio 3

---
+++ exercises/bloco_7/dia_3/2-praticando-tdd/2-2-reescrevendo-funcoes-utilizando-TDD/exercicio-3.js
@@ -0,0 +1,16 @@
+const greaterThanTen = (array) => {
+  let results = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > 10) {
+      results.push(array[i]);
+    }
+  }
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21];
+const result = [32, 21];
+
+const assert = require('assert');
+
+assert.deepStrictEqual(greaterThanTen(parameter), result);
