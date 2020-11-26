084d5a734b70f4a6d2c076708d1407687f243478
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-25 23:29:30 -0300

    Implementacao de testes - Exercicio 5 concluido

---
+++ exercises/bloco_7/dia_3/1-implementacao-de-testes/exercicio-5.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+// implemente seus testes aqui
+assert.deepStrictEqual(obj1, obj2);
+assert.notDeepStrictEqual(obj2, obj3);
