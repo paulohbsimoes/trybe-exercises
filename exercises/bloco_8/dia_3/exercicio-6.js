2d051d7551fae42b3f546524062abbf953338f48
Paulo Simões <paulohbsimoes@gmail.com>
2020-12-01 16:50:07 -0300

    Exercicio 6 concluido

---
+++ exercises/bloco_8/dia_3/exercicio-6.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const alunos = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const notas = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  return alunos.map((aluno, index) => ({
+    name: aluno,
+    average: notas[index].reduce((total, nota) => total + nota) / notas[index].length
+  }))
+}
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepEqual(studentAverage(), expected);
