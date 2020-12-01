cd582813f77e91e9d56e2b0330929f2001caba37
Paulo Simões <paulohbsimoes@gmail.com>
2020-12-01 16:50:00 -0300

    Exercicio 5 concluido

---
+++ exercises/bloco_8/dia_3/exercicio-5.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, cur) => acc + cur.match(/a/gi).length, 0);
+}
+
+assert.deepEqual(containsA(), 20);
