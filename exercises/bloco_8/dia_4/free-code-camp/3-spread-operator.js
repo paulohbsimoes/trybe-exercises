cf146957741ad73c33e5d2699aa804017d9ed9f4
Paulo Simões <paulohbsimoes@gmail.com>
2020-12-02 09:10:40 -0300

    Exercicios freeCodeCamp conluidos

---
+++ exercises/bloco_8/dia_4/free-code-camp/3-spread-operator.js
@@ -0,0 +1,6 @@
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
---
+++ exercises/bloco_8/dia_4/free-code-camp/7-destructuring-assignment-4.js
@@ -0,0 +1,3 @@
+let a = 8, b = 6;
+// Only change code below this line
+[a, b] = [b, a];
