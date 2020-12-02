e7b352385bf29b28a78f86e3257eeb1185b422e6
Paulo Simões <paulohbsimoes@gmail.com>
2020-12-02 08:43:24 -0300

    Exercicio 6 concluido

---
+++ exercises/bloco_8/dia_4/exercicio-6.js
@@ -0,0 +1,12 @@
+const assert = require('assert')
+
+const palio = ["Palio", "Fiat", 2019]
+const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
+const chiron = ["Chiron", "Bugatti", 2016]
+
+// escreva toObject abaixo
+const toObject = ([ name, brand, year ]) => ({ name, brand, year });
+
+assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
+assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
+assert.deepEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
