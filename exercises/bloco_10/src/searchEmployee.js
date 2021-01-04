ff5a8b3c941170d8ea303d5ee4a5b095f6311984
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-04 15:37:21 -0300

    Bonus - Exercicio concluido

---
+++ exercises/bloco_10/src/searchEmployee.js
@@ -0,0 +1,48 @@
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+// Pesquisa
+const searchEmployee = (id, detail) => {
+  const result = professionalBoard.find(professional => professional.id === id);
+  if (result === undefined) return 'ID não identificada';
+  if (result[detail] === undefined) return 'Informação indisponível'
+  return result[detail];
+};
+
+module.exports = searchEmployee;
