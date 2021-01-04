3b84b8a815dceba30e8416188223865dc79c3c90
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-04 13:48:32 -0300

    Parte 1 - Exercicio 3 concluido

---
+++ exercises/bloco_10/src/myRemoveWithoutCopy.js
@@ -0,0 +1,12 @@
+function myRemoveWithoutCopy(arr, item) {
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i -= 1;
+      len -= 1;
+    }
+  }
+  return arr;
+}
+
+module.exports = myRemoveWithoutCopy;
