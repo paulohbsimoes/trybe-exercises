ea28d27b2505b10afd8a7e6431068c100d161ee3
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-04 13:48:08 -0300

    Parte 1 - Exercicio 2 concluido

---
+++ exercises/bloco_10/src/myRemove.js
@@ -0,0 +1,11 @@
+function myRemove(arr, item) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+module.exports = myRemove;
