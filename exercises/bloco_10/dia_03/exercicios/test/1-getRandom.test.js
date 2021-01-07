3a989fd7ce7266988e044d02afb0df59d0d6a7e3
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-07 10:15:30 -0300

    Exercício 3 concluído

--- exercises/bloco_10/dia_03/exercicios/test/1-getRandom.test.js
+++ exercises/bloco_10/dia_03/exercicios/test/1-getRandom.test.js
@@ -30,4 +30,31 @@ describe('getRandom', () => {
     expect(spy).toHaveLastReturnedWith(undefined);
     expect(spy).toBeCalledTimes(2);
   });
+
+  it('Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro', () => {
+    spy.mockImplementation((...args) => args.reduce((acc, cur) => acc * cur));
+
+    getRandom.getRandom(10, 2, 2);  
+    expect(spy).toHaveBeenLastCalledWith(10, 2, 2);
+    expect(spy).toHaveLastReturnedWith(40);
+    expect(spy).toBeCalledTimes(1);
+
+    getRandom.getRandom(2, 2, 2);  
+    expect(spy).toHaveBeenLastCalledWith(2, 2, 2);
+    expect(spy).toHaveLastReturnedWith(8);
+    expect(spy).toBeCalledTimes(2);
+
+    spy.mockReset();
+    spy.mockImplementation(x => x * 2);
+
+    getRandom.getRandom(10);
+    expect(spy).toHaveBeenLastCalledWith(10);
+    expect(spy).toHaveLastReturnedWith(20);
+    expect(spy).toBeCalledTimes(1);
+
+    getRandom.getRandom(77);
+    expect(spy).toHaveBeenLastCalledWith(77);
+    expect(spy).toHaveLastReturnedWith(154);
+    expect(spy).toBeCalledTimes(2);
+  });
 });
