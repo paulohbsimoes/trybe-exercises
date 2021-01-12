d6b4bf5f4afff2eb9a4a5a1eb439b2f734e0ad50
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-12 10:47:45 -0300

    Para fixar - exercícios concluídos

---
+++ exercises/bloco_11/dia_01/para-fixar/meu-app/src/App.js
@@ -0,0 +1,7 @@
+import Bio from './Component';
+
+function App() {
+  return <Bio />;
+}
+
+export default App;
---
+++ exercises/bloco_11/dia_01/para-fixar/meu-app/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Bio extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Paulo Simões</h1>
+        <p>Um cara em busca de novos conhecimentos.</p>
+      </div>
+    );
+  }
+}
+
+export default Bio;
