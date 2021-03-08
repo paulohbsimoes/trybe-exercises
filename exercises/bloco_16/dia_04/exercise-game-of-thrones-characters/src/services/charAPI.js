64572711b45c6a17c82cff4ce0883223f82aef76
Paulo Simões <paulohbsimoes@gmail.com>
2021-03-08 13:42:07 -0300

    Exercise Game of Thrones Characters - concluído

---
+++ exercises/bloco_16/dia_04/exercise-game-of-thrones-characters/src/services/charAPI.js
@@ -0,0 +1,21 @@
+const APIURL = 'https://anapioficeandfire.com/api/characters?name='
+
+const charAPI = (char) => (
+  fetch(`${APIURL}${char.split().join('+')}`)
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error();
+    })
+    .then((data) => data)
+);
+
+// const charAPI = (char) => (
+//   fetch(`${APIURL}${char.split().join('+')}`)
+//     .then((response) => (
+//       response
+//         .json()
+//         .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+//     ))
+// );
+
+export default charAPI;
