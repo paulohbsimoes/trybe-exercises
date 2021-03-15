cfc08f833a99ebec4f5834a1e5b26b4c710efdc1
Paulo Simões <paulohbsimoes@gmail.com>
2021-03-15 11:54:25 -0300

    Exercício 2.1 concluído

---
+++ exercises/bloco_18/dia_03/exercise-hooks-useEffect-customHooks-exercise-two.one/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Greeting() {
+  const [name, setName] = useState('')
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" value={name} />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  );
+}
+
+function App() {
+  return <Greeting />;
+}
+
+export default App;
