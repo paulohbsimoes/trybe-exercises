ed0ae3b297fc04f7a7a4de596a02d5748c5442cb
Paulo Simões <paulohbsimoes@gmail.com>
2021-05-14 16:47:44 -0300

    Exercício 5 concluído

--- exercises/bloco_26/dia_01/my-scripts/imc.js
+++ exercises/bloco_26/dia_01/my-scripts/imc.js
@@ -1,10 +1,19 @@
 const readline = require('readline-sync');
 
 const PESO = readline.questionFloat('Qual seu peso? ');
-const ALTURA = readline.questionFloat('Qual sua altura? ');;
+const ALTURA = readline.questionFloat('Qual sua altura? ');
+
+function getSituation(imc) {
+  if (imc < 18.5) return 'Abaixo do peso (magreza)';
+  if (imc < 25) return 'Peso normal';
+  if (imc < 30) return 'Acima do peso (sobrepeso)';
+  if (imc < 35) return 'Obesidade grau I ';
+  if (imc < 40) return 'Obesidade grau II ';
+  return 'Obesidade graus III e IV';
+}
 
 function getIMC(peso, altura) {
   return peso / (altura ** 2);
 }
 
-console.log(getIMC(PESO, ALTURA));
+console.log(`${getIMC(PESO, ALTURA)}, ${getSituation(getIMC(PESO, ALTURA))}`);
