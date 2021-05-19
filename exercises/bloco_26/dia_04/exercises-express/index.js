c066f018b55ddffe547a2fddcc1fa9eb4d5de0d2
Paulo Simões <paulohbsimoes@gmail.com>
2021-05-19 19:25:44 -0300

    Exercício 2 concluído

--- exercises/bloco_26/dia_04/exercises-express/index.js
+++ exercises/bloco_26/dia_04/exercises-express/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log(`Listening on port 3000`)
 });
@@ -8,3 +10,8 @@ app.listen(3000, () => {
 app.get('/ping', (_req, res) => {
   res.send({ message: 'pong' });
 });
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.send({ message: `Hello, ${name}!` })
+})
