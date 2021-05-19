708f1adfb60d46ada59189cf3f01d955da805c1d
Paulo Simões <paulohbsimoes@gmail.com>
2021-05-19 13:39:11 -0300

    Exercício 4 concluído

---
+++ exercises/bloco_26/dia_03/test-exercises/tests/exercise-4.test.js
@@ -0,0 +1,20 @@
+const writeToFile = require('../src/writeToFile');
+const { expect } = require('chai');
+const uuid = require('node-uuid')
+
+describe('A função "writeToFile" deve', () => {
+  it('lançar um error quando receber um número de argumentos diferente de 2', async () => {
+    try {
+      await writeToFile();
+    } catch (error) {
+      expect(error).to.match(/Número de argumentos inválido/);
+    }
+  })
+
+  it('retornar "ok" após realizar a escrita no arquivo', async () => {
+    const FILEPATH = `./teste-${uuid()}.txt`;
+    const CONTENT = "CONTEÚDO DO ARQUIVO";
+    const result = await writeToFile(FILEPATH, CONTENT);
+    expect(result).to.be.equal('ok');
+  })
+})
