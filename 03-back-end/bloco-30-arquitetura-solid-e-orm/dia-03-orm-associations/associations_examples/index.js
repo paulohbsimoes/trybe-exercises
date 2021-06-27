69c7ea3b0a73938dd12cb23eef6bcc8eb91033dc
Paulo Simões <paulohbsimoes@gmail.com>
2021-06-27 17:27:03 -0300

    Adiciona testes

--- 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/index.js
+++ 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/index.js
@@ -5,7 +5,11 @@ const { Address, Employee } = require('./models');
 const { Book, User } = require('./models');
 const config = require('./config/config');
 
-const sequelize = new Sequelize(config.development);
+// const sequelize = new Sequelize(config.development);
+
+const sequelize = new Sequelize(
+  process.env.NODE_ENV === 'test' ? config.test : config.development
+);
 
 const app = express();
 
@@ -64,65 +68,70 @@ app.get('/usersbooks/:id', async (req, res) => {
 
 
 // UNMANAGED TRANSACTIONS
-// app.post('/employees', async (req, res) => {
-//   // Primeiro iniciamos a transação
-//   const t = await sequelize.transaction();
-
-//   try {
-//     const { firstName, lastName, age, city, street, number } = req.body;
-
-//     // Depois executamos as operações
-//     const employee = await Employee.create(
-//       { firstName, lastName, age },
-//       { transaction: t },
-//     );
-
-//     await Address.create(
-//       { city, street, number, employee_id: employee.id },
-//       { transaction: t },
-//     );
-
-//     // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
-//     // Com isso, podemos finalizar a transação usando a função `commit`.
-//     await t.commit();
-
-//     return res.status(201).json({ message: 'Cadastrado com sucesso' });
-//   } catch (e) {
-//     // Se entrou nesse bloco é porque alguma operação falhou.
-//     // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
-//     await t.rollback();
-//     console.log(e.message);
-//     res.status(500).json({ message: 'Algo deu errado' });
-//   }
-// });
-
-//MANAGED TRANSACTIONS
 app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
   try {
     const { firstName, lastName, age, city, street, number } = req.body;
 
-    const result = await sequelize.transaction(async (t) => {
-      const employee = await Employee.create({
-        firstName, lastName, age
-      }, { transaction: t });
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
 
-      await Address.create({
-        city, street, number, employee_id: employee.id
-      }, { transaction: t });
+    await Address.create(
+      { city, street, number, employee_id: employee.id },
+      { transaction: t },
+    );
 
-      return res.status(201).json({ message: 'Cadastrado com sucesso' });
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    // return res.status(201).json({ message: 'Cadastrado com sucesso' });
+
+    return res.status(201).json({
+      id: employee.id, // esse dado será nossa referência para validar a transação
+      message: 'Cadastrado com sucesso'
     });
-
-    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
-    // não sendo necessário finalizar a transação manualmente.
-    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
   } catch (e) {
     // Se entrou nesse bloco é porque alguma operação falhou.
     // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   }
 });
+
+//MANAGED TRANSACTIONS
+// app.post('/employees', async (req, res) => {
+//   try {
+//     const { firstName, lastName, age, city, street, number } = req.body;
+
+//     const result = await sequelize.transaction(async (t) => {
+//       const employee = await Employee.create({
+//         firstName, lastName, age
+//       }, { transaction: t });
+
+//       await Address.create({
+//         city, street, number, employee_id: employee.id
+//       }, { transaction: t });
+
+//       return res.status(201).json({ message: 'Cadastrado com sucesso' });
+//     });
+
+//     // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+//     // não sendo necessário finalizar a transação manualmente.
+//     // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+//   } catch (e) {
+//     // Se entrou nesse bloco é porque alguma operação falhou.
+//     // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+//     console.log(e.message);
+//     res.status(500).json({ message: 'Algo deu errado' });
+//   }
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
---
+++ 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/tests/integration/employeeCreation.test.js
@@ -0,0 +1,128 @@
+// Testando a rota POST /employees
+// - Acessarei meu `rest-client` favorito (PostMan, Insomnia, HTTPie, etc...)
+//   - Para um caso de sucesso:
+//     - Farei uma requisição `POST` com os dados corretos para meu
+//       end-point `/employee`;
+//     - Aguardo uma resposta com status `201 - Created`;
+//     - Essa resposta deve conter também um atributo `id`, no corpo;
+//     - Essa resposta deve conter também um atributo `message`,
+//       no corpo, com a mensagem `Cadastrado com sucesso`;
+//     - Farei uma requisição `GET` utilizando esse `id` para meu
+//       end-point `/employee/:id`;
+//     - Aguardo uma resposta com status `200 - OK`;
+//     - Essa resposta deve conter também um atributo `addresses`,
+//       no corpo, com pelo menos um item.
+//   - Para um caso de falha:
+//     - Farei uma requisição `POST` com os dados incorretos para meu
+//       end-point `/employee`;
+//     - Aguardo uma resposta com status `500 - Internal Server Error`;
+//     - Essa resposta deve conter também um atributo `message`,
+//       no corpo, com a mensagem `Algo deu errado`;
+
+const chai = require('chai');
+const { stub } = require('sinon');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const app = require('../../index');
+
+// omitir os `console.log`s dos testes gerando um `stub` pra função
+const consoleLogStub = stub(console, 'log');
+before(()=> consoleLogStub.returns(true));
+after(()=> consoleLogStub.restore());
+
+describe('Rota POST /employees', () => {
+  describe('quando os dados do `body` são válidos', () => {
+    let postEmployee;
+    let getEmployee;
+
+    before(async () => {
+      try {
+        postEmployee = await chai.request(app)
+          .post('/employees')
+          .send({
+            firstName: "Rodrigo",
+            lastName: "Oliveira",
+            age: 30,
+            city: "TrybeCity",
+            street: "Rua Teste",
+            number: 42
+          });
+
+        const { body : { id } } = postEmployee;
+
+        getEmployee = await chai.request(app)
+          .get(`/employees/${id}?includeAddresses=true`);
+      } catch (error) {
+        console.error(error.message);
+      }
+    });
+
+    it('retorna 201 - Created', async () => {
+      const { status } = postEmployee;
+
+      expect(status).to.be.equals(201);
+    });
+
+    it('retorna um atributo `id`, que é um número', async () => {
+      const { body: { id } } = postEmployee;
+
+      expect(typeof id).to.be.equals("number");
+    });
+
+    it('retorna uma mensagem `Cadastrado com sucesso`', async () => {
+      const { body: { message } } = postEmployee;
+
+      expect(message).to.be.equals('Cadastrado com sucesso');
+    });
+
+    it('é possível consultar a pessoa criada através do `id` retornado', async () => {
+      const { body: { id: postId } } = postEmployee;
+      const { body: { employee: { id: getId } } } = getEmployee;
+
+      expect(postId).to.be.equals(getId);
+    });
+
+    it('essa consulta também retornou um atributo `addresses`, com pelo menos um item', async () => {
+      const { body: { addresses } } = getEmployee;
+
+      expect(addresses.length).to.be.greaterThanOrEqual(1);
+    });
+  });
+
+  describe('quando os dados do `body` não são válidos', () => {
+    let postEmployee;
+
+    before(async () => {
+      try{
+        // removendo city
+        postEmployee = await chai.request(app)
+          .post('/employees')
+          .send({
+            firstName: "Rodrigo",
+            lastName: "Oliveira",
+            age: 30,
+            street: "Rua Teste",
+            number: 42
+          });
+      } catch (error) {
+        console.error(error.message);
+      }
+    });
+
+    it('retorna 500 - Internal Server Error', async () => {
+      const { status } = postEmployee;
+
+      expect(status).to.be.equals(500);
+    });
+
+    it('retorna uma mensagem `Algo deu errado`', async () => {
+      const { body: { message } } = postEmployee;
+
+      expect(message).to.be.equals('Algo deu errado');
+    });
+  });
+});
