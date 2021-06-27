8320bb766584f27e4e5146d9364b0a357339efee
Paulo Simões <paulohbsimoes@gmail.com>
2021-06-27 15:46:39 -0300

    Adiciona conteúdo lazy loading

--- 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/index.js
+++ 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/index.js
@@ -19,15 +19,15 @@ app.get('/employees', async (_req, res) => {
 app.get('/employees/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const employee = await Employee.findOne({
-      where: { id },
-      include: [{
-        model: Address, as: 'addresses', attributes: { exclude: ['number'] },
-      }],
-    });
+    const employee = await Employee.findOne({ where: { id } });
 
     if (!employee)
       return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await employee.getAddresses();
+      return res.status(200).json({ employee, addresses });
+    }
 
     return res.status(200).json(employee);
   } catch (e) {
