e656959df3cbc1cf700e428915991e346973457a
Paulo Simões <paulohbsimoes@gmail.com>
2021-06-27 15:37:31 -0300

    Adiciona conteúdo eager loading

--- 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/index.js
+++ 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/index.js
@@ -16,6 +16,26 @@ app.get('/employees', async (_req, res) => {
   };
 });
 
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+      where: { id },
+      include: [{
+        model: Address, as: 'addresses', attributes: { exclude: ['number'] },
+      }],
+    });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
 
--- 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/seeders/20210627182358-addresses.js
+++ 03-back-end/bloco-30-arquitetura-solid-e-orm/dia-03-orm-associations/associations_examples/seeders/20210627182358-addresses.js
@@ -4,7 +4,7 @@ module.exports = {
       [
         {
           city: 'Belo Horizonte',
-          street: 'Rua Flórida',
+          street: 'Rua Florida',
           number: 1080,
           employee_id: 1,
         },
@@ -27,10 +27,16 @@ module.exports = {
           employee_id: 4,
         },
         {
+          city: 'Belo Horizonte',
+          street: 'Rua Vicente Alvarenga',
+          number: 80,
+          employee_id: 1,
+        },
+        {
           city: 'Curitiba',
           street: 'Rua Fria',
           number: 101,
-          employee_id: 4,
+          employee_id: 5,
         },
       ],
       {},
