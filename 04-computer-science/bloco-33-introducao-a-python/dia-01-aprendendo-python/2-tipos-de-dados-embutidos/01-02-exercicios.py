849d893badf4479f620cd44815d84c82384928a5
Paulo Simões <paulohbsimoes@gmail.com>
2021-08-11 09:18:43 -0300

    feat(33.1): add tipos-de-dados-embutidos

---
+++ 04-computer-science/bloco-33-introducao-a-python/dia-01-aprendendo-python/2-tipos-de-dados-embutidos/01-02-exercicios.py
@@ -0,0 +1,13 @@
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+
+trybe_course.append("Computer Science")
+
+print("Lista: %s" % trybe_course)
+
+print("Valor da primeira posição: %s" % trybe_course[0])
+
+trybe_course[0] = "Fundamentos"
+
+print("Valor da primeira posição após sobrescrever: %s" % trybe_course[0])
+
+print("Lista atualizada: %s" % trybe_course)
