8cc54ce78184ee5efbf524531994953f1506c8dd
Paulo Simões <paulohbsimoes@gmail.com>
2021-08-14 08:42:06 -0300

    feat(33.3): add dubles-de-teste

---
+++ 04-computer-science/bloco-33-introducao-a-python/dia-03-testes/3-dubles-de-teste/pokemon_1.py
@@ -0,0 +1,12 @@
+import json
+
+
+def retrieve_pokemons_by_type(type, reader):
+    # lê o conteudo de reader e o transforma de json
+    # para dicionário
+    pokemons = json.load(reader)["results"]
+    # filtra somente os pokemons do tipo escolhido
+    pokemons_by_type = [
+        pokemon for pokemon in pokemons if type in pokemon["type"]
+    ]
+    return pokemons_by_type
